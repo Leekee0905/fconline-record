072997f684498f76285936c88c6fa06a4861863e
GiSeong Lee <cj8928@gmail.com>
2024-08-24 13:02:10 +0900

    project: fconline 전적검색 사이트 시작

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fco.dn.nexoncdn.co.kr",
+        port: "/**",
+      },
+    ],
+    formats: ["image/webp"],
+  },
+};
 
 export default nextConfig;
