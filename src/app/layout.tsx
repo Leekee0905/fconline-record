ea423a8041ff0279ce7ce96cd39d678b3e634ef4
GiSeong Lee <cj8928@gmail.com>
2024-08-25 16:55:53 +0900

    add: react-query provider생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import QueryProvider from "./lib/QueryProvider";
 
 export const metadata: Metadata = {
   title: "MyFC",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-korean bg-black text-white">{children}</body>
+      <body className="font-korean bg-black text-white">
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
